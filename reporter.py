52a20c903a3c00bbeb4637db237741788b2f6606
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-24 07:33:11 +1000

    streamline

--- reporter.py
+++
@@ -1,8 +0,0 @@
-"""Report theme utilities."""
-import json
-
-DEFAULT_THEME = "default"
-DEFAULT_CHARACTERS = 101
-
-def render(theme=DEFAULT_THEME, characters=DEFAULT_CHARACTERS):
-    return {"theme": theme, "characters": characters}
--- tests/test_render.py
+++
@@ -1,6 +0,0 @@
-from reporter import render
-
-def test_default():
-    r = render()
-    assert r["theme"] == "default"
-    assert r["characters"] == 101
